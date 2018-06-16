fedc14275ce17af787aa8158b1b8e066f4fe035d
Oscar <muhr.oscar@gmail.com>
2018-06-16 10:34:32 +0200

    create initial endpoints to get lights info

---
+++ server/controllers/lights.js
@@ -0,0 +1,36 @@
+const request = require('request');
+
+var lights = {};
+
+lights.getLights = function (req, res) {
+    if(req.params.id) {
+        request.get('http://192.168.8.100' + '/api/' + '6x-bkvv6nNQ8phAferczfhNvRSzvG8j4uvYuJUgW/lights/'+req.params.id,
+            { json: true },
+            function (error, response, body) {
+                res.status(response.statusCode).send(body);
+            });
+    } else {
+        request.get('http://192.168.8.100' + '/api/' + '6x-bkvv6nNQ8phAferczfhNvRSzvG8j4uvYuJUgW/lights',
+        { json: true },
+        function (error, response, body) {
+            res.status(response.statusCode).send(body);
+        });
+    }
+}
+
+lights.toggleLight = function (req, res) {
+    if(req.params.id) {
+        request({
+            url:'http://192.168.8.100' + '/api/' + '6x-bkvv6nNQ8phAferczfhNvRSzvG8j4uvYuJUgW/lights/'+req.params.id+'/state',
+            method: 'PUT',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            json:{'on':false}},
+            function (error, response, body) {
+                res.status(response.statusCode).send(body);
+            });
+    }
+}
+
+module.exports = lights;
