5004f85a5021fbe658fbd9a109430d474554ce83
Sabinatech <me@sabina.tech>
2018-06-16 10:43:59 +0200

    Added toggle-buttons and list items

--- app/app.js
+++ app/app.js
@@ -1,7 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const lights = [1, 2, 3, 4];
+
+class LightState extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLightOn: true };
+    this.handleClick = this.handleClick.bind(this);
+  };
+
+  handleClick() {
+    this.setState(prevState => ({
+      isLightOn: !prevState.isLightOn
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <p> Turn: {this.props.lightName} </p>
+        <button onClick={this.handleClick}>
+          {this.state.isLightOn ? 'ON' : 'OFF'}
+        </button>
+      </div>
+    );
+  }
+}
+
+function LightList(props) {
+  const lights = props.lights;
+  const listLights = lights.map((light) =>
+    <li key={light.toString()}>
+      < LightState lightName={light} />
+    </li>
+  );
+  return (
+    <ul>{listLights}</ul>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1> Welcome to home-smart </h1>
+      <p> An app to make your home smarter </p>
+      < LightList lights={lights} />
+      < MyComponent />
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('root')
 );
