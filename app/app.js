d9bb2db7bdbe57c53c0c4da6599625ac6764f502
Sabinatech <me@sabina.tech>
2018-06-17 10:15:20 +0200

    Brightness slider added

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Slider from 'react-rangeslider';
+import {throttle, debounce} from 'throttle-debounce';
+
+import 'react-rangeslider/lib/index.css';
 
 class LightState extends React.Component {
   constructor(props) {
@@ -14,6 +18,7 @@ class LightState extends React.Component {
         return results.json()
       }).then((result) => {
         this.setState({
+          light: result,
           isLightOn: result.state.on,
           lightName: result.name
         });
@@ -42,20 +47,79 @@ class LightState extends React.Component {
   }
 
   render() {
-    const { error, lightName, isLightOn } = this.state;
-    console.log(isLightOn)
+    const { error, light, lightName, isLightOn } = this.state;
     if (error) {
       return <div> Error: {error.message} </div>;
     } else {
+
       return (
         <div>
           <p> Turn: {lightName} </p>
           <button onClick={this.handleClick}>
             {isLightOn ? 'OFF' : 'ON'}
           </button>
+          {light ? < HorizontalSlider lightState={light} lightNr={this.props.lightNr} /> : 'Loading'}
         </div>
       );
     }
+  }
+}
+
+class HorizontalSlider extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      value: 10
+    };
+    this.handleChange = this.handleChange.bind(this);
+  };
+
+  componentDidMount() {
+    if (this.props.lightState != undefined) {
+      this.setState({ value: this.props.lightState.state.bri });
+    };
+  }
+
+  handleChangeStart() {
+    console.log('Change event started');
+  };
+
+  handleChange(value) {
+    /* Change bri settings on lights */
+    debounce(10, fetch('/api/lights/' + this.props.lightNr + '/brightness', {
+      method: 'PUT',
+      body: JSON.stringify({ brightness: value }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+      .catch(err => err));
+
+    this.setState({
+      value: value
+    });
+  };
+
+  handleChangeComplete() {
+    console.log('Change event completed');
+  };
+
+  render() {
+    const { value } = this.state
+    console.log(value)
+    return (
+      <div className='slider'>
+        <Slider
+          min={0}
+          max={255}
+          value={value}
+          onChangeStart={this.handleChangeStart}
+          onChange={this.handleChange}
+          onChangeComplete={this.handleChangeComplete}
+        />
+        <div className='value'>{value}</div>
+      </div>
+    )
   }
 }
 
